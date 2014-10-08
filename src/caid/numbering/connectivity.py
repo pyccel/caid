a362846ce93f926d54214c44a667a7b1ba36afa1
ARA <ratnaniahmed@gmail.com>
2014-10-08 12:10:16 +0200

    numbering for 1d and 2d without bc validated

--- src/caid/numbering/connectivity.py
+++ src/caid/numbering/connectivity.py
@@ -8,10 +8,13 @@ __all__ = ['connectivity']
 import numpy as np
 array = np.array
 class connectivity:
-    def __init__(self, geometry,ai_ndof=1):
+    def __init__(self, geometry, ai_ndof=1):
+
+        self._geo = geometry
+        self._boundary_condition = None
 
         # computing the number of patchs
-        self.npatch = geometry.npatchs
+        self._npatch = geometry.npatchs
 
         # for each patch, we copy n,p, nel, nen
         self.list_n = []
@@ -81,6 +84,177 @@ class connectivity:
         self.ID_loc  = [[]]*self.npatch
 
         self.list_Elt_Index = []
+
+        self.init_ELT()
+
+    @property
+    def geometry(self):
+        return self._geo
+
+    @property
+    def npatch(self):
+        return self._npatch
+
+    @property
+    def dim(self):
+        return self._geo.dim
+
+    @property
+    def boundary_condition(self):
+        return self._boundary_condition
+
+    def init_ELT_1d(self):
+        geometry = self.geometry
+        dim = self.dim
+        for li_id in range(0, self.npatch):
+            nrb = geometry[li_id]
+            list_info_knots = self.list_info_knots(nrb)
+
+            list_Elt_Index = []
+            # acces a l 'indice du noeud
+            for i in list_info_knots[0][2]:
+                list_Elt_Index.append([i])
+
+            self.list_Elt_Index.append(list_Elt_Index)
+
+    def init_ELT_2d(self):
+        geometry = self.geometry
+        dim = self.dim
+        for li_id in range(0, self.npatch):
+            nrb = geometry[li_id]
+            list_info_knots = self.list_info_knots(nrb)
+
+            list_Elt_Index = []
+            # acces a l 'indice du noeud
+            for j in list_info_knots[1][2]:
+                for i in list_info_knots[0][2]:
+                    list_Elt_Index.append([i, j])
+
+            self.list_Elt_Index.append(list_Elt_Index)
+
+    def init_ELT(self):
+        geometry = self.geometry
+        dim = self.dim
+        if dim == 1:
+            self.init_ELT_1d()
+        if dim == 2:
+            self.init_ELT_2d()
+
+    def init_data_structure_1d(self):
+        geometry = self.geometry
+        dim = self.dim
+        bound_cond = self.boundary_condition
+        for li_id in range(0, self.npatch):
+
+            lpi_Elt_Index = self.list_Elt_Index[li_id]
+            li_nen = self.list_nen[li_id]
+            li_nel = self.list_nel[li_id]
+            lpi_N = self.list_n[li_id]
+            lpi_P = self.list_p[li_id]
+
+            _list_elt_st = self.list_elt_st[li_id]
+            list_elt_st = [ [i for i in _list_elt_st[0] if lpi_P[0]+1<=i and i<=lpi_N[0]]]
+
+            list_IEN  = np.zeros((li_nen, li_nel), dtype=np.int)
+
+            li_baseA = 0
+            li_baseB = 0
+
+            for li_dof in range(0, self.ndof):
+                li_e = 0
+                for li_i in list_elt_st[0]:
+
+                    # A starts from 1
+                    li_A = li_i
+                    li_A = li_A + li_baseA
+                    for li_iloc in range(0, lpi_P[0] + 1):
+
+                        li_B = li_A -(lpi_P[0] - li_iloc)
+                        li_Bloc = li_iloc
+                        li_Bloc = li_Bloc + li_baseB
+
+                        # as A starts from 1, we must deduce 1
+                        list_IEN[li_Bloc, li_e] = ( li_B - 1 )
+
+                    li_e += 1
+
+                li_baseA = li_baseA + li_A
+                li_baseB = li_baseB + li_nen
+
+            self.IEN.append(list_IEN)
+
+    def init_data_structure_2d(self):
+        geometry = self.geometry
+        dim = self.dim
+        bound_cond = self.boundary_condition
+        for li_id in range(0, self.npatch):
+
+            lpi_Elt_Index = self.list_Elt_Index[li_id]
+            li_nen = self.list_nen[li_id]
+            li_nel = self.list_nel[li_id]
+            lpi_N = self.list_n[li_id]
+            lpi_P = self.list_p[li_id]
+
+            _list_elt_st = self.list_elt_st[li_id]
+#            print "list_elt_st ", _list_elt_st
+            list_elt_st = [ [i for i in _list_elt_st[0] if lpi_P[0]+1<=i and i<=lpi_N[0]] \
+                           , [i for i in _list_elt_st[1] if lpi_P[1]+1<=i and i<=lpi_N[1]]]
+
+#            print "li_nel=", li_nel
+            import numpy as np
+            list_IEN  = np.zeros((li_nen, li_nel), dtype=np.int)
+
+            li_baseA = 0
+            li_baseB = 0
+
+            for li_dof in range(0, self.ndof):
+                li_e = 0
+#                print "list_elt_st[0] = ", list_elt_st[0]
+#                print "list_elt_st[1] = ", list_elt_st[1]
+                for li_j in list_elt_st[1]:
+                    for li_i in list_elt_st[0]:
+#                        print "li_i,li_j=", li_i,li_j
+                        # A starts from 1
+                        li_A = (li_j - 1) * lpi_N[0] + li_i
+                        li_A = li_A + li_baseA
+
+                        for li_jloc in range(0, lpi_P[1] + 1):
+
+                            for li_iloc in range(0, lpi_P[0] + 1):
+
+                                li_B = li_A - (lpi_P[1] - li_jloc) * lpi_N[0] -(lpi_P[0] - li_iloc)
+                                li_Bloc = li_jloc * (lpi_P[0] + 1) + li_iloc
+                                li_Bloc = li_Bloc + li_baseB
+
+                                # as A starts from 1, we must deduce 1
+                                list_IEN[li_Bloc, li_e] = li_B - 1
+
+#                        print "list_IEN=", list_IEN[:,li_e]
+                        li_e += 1
+
+                li_baseA = li_baseA + li_A
+                li_baseB = li_baseB + li_nen
+
+            self.IEN.append(list_IEN)
+#        print "list_IEN.shape=", list_IEN.shape
+
+    def init_data_structure(self, bound_cond=None):
+        geometry = self.geometry
+        dim = self.dim
+        if bound_cond is None:
+            import boundary_conditions as bc
+            bound_cond = bc.boundary_conditions(geometry)
+        self._boundary_condition = bound_cond
+        if dim == 1:
+            self.init_data_structure_1d()
+        if dim == 2:
+            self.init_data_structure_2d()
+
+        # INITIALIIZING THE ID ARRAY
+        self.init_ID(bound_cond)
+
+        # INITIALIIZING THE LOCATION MATRIX LM ARRAY
+        self.init_LM()
 
     def init_ID(self, bound_cond):
         list_n = self.list_n
@@ -249,5 +423,22 @@ class connectivity:
         return list_info
 
 
+if __name__ == '__main__':
+
+    import caid.cad_geometry  as cg
+    from caid.cad_geometry import line, square
+    import boundary_conditions as bc
+
+    geo1d = line(n=[5], p=[2])
+    con1d = connectivity(geo1d)
+
+    con1d.init_data_structure()
+    con1d.printinfo()
+
+    geo2d = square(n=[5,3], p=[2,3])
+    con2d = connectivity(geo2d)
+
+    con2d.init_data_structure()
+    con2d.printinfo()
 
 
