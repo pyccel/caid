19e1e2ce46cf609d1dbeffbaf9042e7f90ae3f0d
ratnani ahmed <ratnaniahmed@gmail.com>
2015-08-13 20:07:22 +0200

    add save function for cubic hermite bezier

--- tests/test_hbezier_mesh.py
+++ tests/test_hbezier_mesh.py
@@ -3,18 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from caid.quadrangles.hermite_bezier import rectangle, square, circle
 
-geo = rectangle(n=[25,25], origin=[5.,4.], lengths=[1.,2.])
-geo.plot()
-plt.title("Rectangular domain using Cubic hermite bezier elements")
-plt.show()
+#geo = rectangle(n=[25,25], origin=[5.,4.], lengths=[1.,2.])
+#geo.plot()
+#plt.title("Rectangular domain using Cubic hermite bezier elements")
+#plt.show()
 
-geo = square(n=[25,25])
-geo.plot()
-plt.title("Square domain using Cubic hermite bezier elements")
-plt.show()
+geo = square(n=[15,15])
+geo.save(label="jorek")
+#geo.plot()
+#plt.title("Square domain using Cubic hermite bezier elements")
+#plt.show()
 
-geo = circle(n=[25,25], center=[5.,4.], rmin=0.1, rmax=1.5)
-geo.plot()
-plt.title("Circular domain using Cubic hermite bezier elements")
-plt.show()
-
+#geo = circle(n=[25,25], center=[5.,4.], rmin=0.1, rmax=1.5)
+#geo.plot()
+#plt.title("Circular domain using Cubic hermite bezier elements")
+#plt.show()
